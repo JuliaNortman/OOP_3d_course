68dab66388339afbec39350ae22fdba3c897ca04
JuliaNortman <ynortman@gmail.com>
2020-06-13 12:16:18 +0300

    Delete Main.java

--- Problems/7ReadersWriterLock/src/com/Main.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Main {
-
-    public static void main(String[] args) {
-        ReadersWriterLockExample example = new ReadersWriterLockExample();
-        example.run();
-    }
-}
